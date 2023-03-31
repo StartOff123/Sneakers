aa70834c2523bdeb07fb3c686913e8cef00214a7
Студент <student@ad.anosov.ru>
2023-03-31 09:49:34 +0500

    all card finish, add redux

--- sneakers-frоntend/src/components/Card/index.jsx
+++ sneakers-frоntend/src/components/Card/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Heart, Plus } from 'react-bootstrap-icons'
+import { Heart, Plus, HeartFill, Check } from 'react-bootstrap-icons'
+import classnames from 'classnames'
 
 import sneacers from '../../assets/img/sneakers.jpg'
 import './Card.scss'
 
 const Card = () => {
+    const [liked, setLiked] = React.useState(false)
+    const [added, setAdded] = React.useState(false)
+
     return (
         <div className='card'>
             <div className="card__img">
-                <button>
-                    <Heart />
-                </button>
+                <div onClick={() => setLiked(!liked)} className={classnames('card__img--likeBtn', {
+                    'card__img--likeBtn-liked': liked
+                })}>
+                    {liked ? <HeartFill color='#FF8585'/> : <Heart />}
+                </div>
                 <img src={sneacers} alt="Sneakers" />
             </div>
             <div className="card__title">
@@ -21,9 +27,11 @@ const Card = () => {
                     <b>Цена:</b>
                     <p>12 999 ₽</p>
                 </div>
-                <button>
-                    <Plus />
-                </button>
+                <div onClick={() => setAdded(!added)} className={classnames('card__bottom--addBtn', {
+                    'card__bottom--addBtn-added': added
+                })}>
+                    {added ? <Check  color='#fff'/> : <Plus />}
+                </div>
             </div>
         </div>
     )
--- sneakers-frоntend/src/pages/Home/index.jsx
+++ sneakers-frоntend/src/pages/Home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Header from '../../components/Header'
 import AllSneacers from '../../modules/AllSneakers'
+import Cart from '../../modules/Cart'
 import './Home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
+      <Cart />
       <Header />
       <AllSneacers />
     </div>
